1cb281f89290c47c7d074d364878a44e0fe63ea6
Franco Fadini <franco@made2.co>
2018-10-13 14:09:14 -0300

    Initial Commit

---
+++ BaseProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BaseProject
+//
+//  Created by Franco Fadini on 13/10/18.
+//  Copyright © 2018 made2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
