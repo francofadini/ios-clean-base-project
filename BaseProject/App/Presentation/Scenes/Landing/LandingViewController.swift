95dd65a57154ab6f55a9788c3e7247f0a929d0de
Franco Fadini <franco@made2.co>
2018-12-17 10:42:55 -0300

    Add landing view

---
+++ BaseProject/App/Presentation/Scenes/Landing/LandingViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class LandingViewController: UIViewController {
+
+  @IBOutlet weak var logoImageView: UIImageView!
+  @IBOutlet weak var loginButton: UIButton!
+  @IBOutlet weak var registerButton: UIButton!
+
+  override func viewDidLoad() {
+        super.viewDidLoad()
+  }
+}
