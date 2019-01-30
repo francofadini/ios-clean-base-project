6690b09ce4bf4b402b770f96e17078f074c8c1f1
Franco Fadini <franco@made2.co>
2019-01-30 01:17:05 -0300

    simple reformat for ui library

--- BaseProject/App/Presentation/UI Atomic Library/Atoms/Form Inputs Atoms/DateInputRowAtom.swift
+++ BaseProject/App/Presentation/UI Atomic Library/Atoms/Form Inputs Atoms/DateInputRowAtom.swift
@@ -2,16 +2,16 @@ import Foundation
 import UIKit
 
 public class DateInputRowAtom: InputRow<Date> {
-
+  
   public var data = DateInputData()
   public var style: DateInputStyle = .defaultStyle
   public var onDoneAction: ((_ newDate: Date) -> Void)?
-
+  
   private var label = UILabel()
   private var detailLabel = UILabel()
   private var datePicker = UIDatePicker()
   private var dummyTextField = UITextField()
-
+  
   override public func layoutSubviews() {
     super.layoutSubviews()
     preConfigureSubviews()
@@ -20,17 +20,17 @@ public class DateInputRowAtom: InputRow<Date> {
     postConfigureSubviews()
     buildAtom()
   }
-
+  
   private func preConfigureSubviews() {
-
+    
     self.didTapHandler = {
       self.onRowSelected()
     }
-
+    
     self.dummyTextField.inputView = datePicker
     addDoneButtonOnKeyboard()
   }
-
+  
   private func onRowSelected() {
     if self.dummyTextField.isFirstResponder {
       self.dummyTextField.resignFirstResponder()
@@ -38,43 +38,43 @@ public class DateInputRowAtom: InputRow<Date> {
       self.dummyTextField.becomeFirstResponder()
     }
   }
-
+  
   private func addDoneButtonOnKeyboard() {
     let doneToolbar: UIToolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: 320, height: 50))
     doneToolbar.barStyle = UIBarStyle.default
     doneToolbar.tintColor = UIApplication.shared.keyWindow?.tintColor
-
+    
     let flexSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace,
                                     target: nil,
                                     action: nil)
-
+    
     let done: UIBarButtonItem = UIBarButtonItem(title: "OK",
                                                 style: UIBarButtonItem.Style.done,
                                                 target: self,
                                                 action: #selector(onDoneButtonPressed))
-
+    
     var items = [UIBarButtonItem]()
     items.append(flexSpace)
     items.append(done)
-
+    
     doneToolbar.items = items
     doneToolbar.sizeToFit()
-
+    
     self.dummyTextField.inputAccessoryView = doneToolbar
   }
-
+  
   @objc private func onDoneButtonPressed() {
-
+    
     self.value = self.datePicker.date
     bindDateOnDetailLabel(date: self.value!)
-
+    
     if self.dummyTextField.isFirstResponder {
       self.dummyTextField.resignFirstResponder()
     }
-
+    
     onDoneAction?(self.value!)
   }
-
+  
   private func bindData() {
     self.label.text = data.label
     if let value = self.value {
@@ -84,13 +84,13 @@ public class DateInputRowAtom: InputRow<Date> {
       self.detailLabel.text = "-"
     }
   }
-
+  
   private func bindDateOnDetailLabel(date: Date) {
     let dateFormatter = DateFormatter()
     dateFormatter.dateFormat = style.dateFormat
     self.detailLabel.text = dateFormatter.string(from: date)
   }
-
+  
   private func applyStyle() {
     self.label.textColor = style.labelTextColor
     self.label.font = style.labelFont
@@ -99,12 +99,12 @@ public class DateInputRowAtom: InputRow<Date> {
     self.detailLabel.textAlignment = .right
     self.datePicker.datePickerMode = style.datePickerMode
   }
-
+  
   private func postConfigureSubviews() {
     self.label.sizeToFit()
     self.label.widthAnchor.constraint(equalToConstant: self.label.frame.width).isActive = true
   }
-
+  
   private func buildAtom() {
     self.addSubview(self.dummyTextField)
     let horizontalStack = UIStackView(arrangedSubviews: [self.label, self.detailLabel])
@@ -125,14 +125,14 @@ public class DateInputStyle {
   let detailLabelFont: UIFont?
   let dateFormat: String
   let datePickerMode: UIDatePicker.Mode
-
+  
   public init(labelTextColor: UIColor? = nil,
-       labelFont: UIFont? = nil,
-       detailLabelTextColor: UIColor? = nil,
-       detailLabelFont: UIFont? = nil,
-       dateFormat: String,
-       datePickerMode: UIDatePicker.Mode) {
-
+              labelFont: UIFont? = nil,
+              detailLabelTextColor: UIColor? = nil,
+              detailLabelFont: UIFont? = nil,
+              dateFormat: String,
+              datePickerMode: UIDatePicker.Mode) {
+    
     self.labelTextColor = labelTextColor
     self.labelFont = labelFont
     self.detailLabelTextColor = detailLabelTextColor
@@ -140,9 +140,9 @@ public class DateInputStyle {
     self.dateFormat = dateFormat
     self.datePickerMode = datePickerMode
   }
-
+  
   // MARK: Default styles
-
+  
   public static var defaultStyle: DateInputStyle {
     return DateInputStyle(labelTextColor: .black,
                           detailLabelTextColor: .darkGray,
