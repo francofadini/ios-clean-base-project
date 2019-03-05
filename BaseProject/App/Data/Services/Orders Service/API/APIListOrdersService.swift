b36be2c40d7f9d5803ebd95fa81efef5f90fdd48
Franco Fadini <francofadini@gmail.com>
2019-03-05 20:36:57 -0300

    rename list orders service file

--- BaseProject/App/Data/Services/Orders Service/API/APIListOrdersService.swift
+++ BaseProject/App/Data/Services/Orders Service/API/APIListOrdersService.swift
@@ -1,17 +1,6 @@
 // swiftlint:disable identifier_name
 import Foundation
 
-// MARK: API MODELS
-
-struct APICreateOrderRequest: Codable {
-  let title: String
-}
-
-struct APICreateOrderResponse: Codable {
-  let id: Int
-  let title: String
-}
-
 class APIListOrdersService: ListOrdersService {
   func listOrdersWith(clientID: Int?, success: ([Order]) -> Void, failure: (ListOrdersError) -> Void) {
     
